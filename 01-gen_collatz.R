657314082a06a54fa186d4bd2de975e1253af085
Farzy Hafizam <141115682+farzyhafizam@users.noreply.github.com>
2023-09-24 20:10:52 +0800

    Update 01-gen_collatz.R
    added tibble

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -11,3 +11,33 @@ while(n != 1) {
   n <- gen_collatz(n)
   print(n)
 }
+
+library(tibble)
+
+gen_collatz <- function(n) {
+  if (n <= 0) {
+    cat("Invalid input. Please enter a positive integer.\n")
+    return(NULL)
+  }
+  
+  sequence <- c(n)
+  while (n > 1) {
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else{n <- 3 * n + 1}
+    sequence <- c(sequence, n)
+  }
+  
+  return(sequence)
+}
+
+collatz_df <- tibble(start = numeric(0), seq = list())
+
+for (i in 1:10000) {
+  collatz_seq <- gen_collatz(i)
+  if (!is.null(collatz_seq)) {
+    collatz_df <- collatz_df %>% add_row(start = i, seq = list(collatz_seq))
+  }
+}
+
+collatz_df
