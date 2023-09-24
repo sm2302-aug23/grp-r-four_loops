f8075eec5a9981038d217b27d5c2925a853437e9
Faathimahali <22b9015@ubd.edu.bn>
2023-09-24 18:30:03 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -1,16 +1,13 @@
-library(tiddyverse)
+library(tidyverse)
 
-n <- 10
+n <- 3
 
 gen_collatz <- function(n){
-  if (n %% 2 == 0) {n/2}
-  else {3 * n + 1}
+    if (n %% 2 == 0) {n/2}
+    else {3 * n + 1}
 }
 
-
-n <- 10
-
-while (n < 15) {
-  n <- n + 1
-  print (n)
+while(n != 1) {
+  n <- gen_collatz(n)
+  print(n)
 }
