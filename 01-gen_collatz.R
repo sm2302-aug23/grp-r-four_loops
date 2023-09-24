0c622169ed9852b338eaab3446dcbd0820d21500
Faathimahali <22b9015@ubd.edu.bn>
2023-09-24 21:11:56 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -1,19 +1,6 @@
 #Task 1 -----------------------------------
 
 library(tidyverse)
-
-n <- 3
-
-gen_collatz <- function(n){
-    if (n %% 2 == 0) {n/2}
-    else {3 * n + 1}
-}
-
-while(n != 1) {
-  n <- gen_collatz(n)
-  print(n)
-}
-
 library(tibble)
 
 gen_collatz <- function(n) {
@@ -47,20 +34,4 @@ collatz_df <- tibble(
   
 collatz_df
 
-
-#Task 2 --------------------------------
-
-library(tidyverse)
-
-top10longest <- collatz_df %>%
-  arrange(desc(length)) %>%
-  slice(1:10)
-
-top10longest
-
-max_val_int <- collatz_df %>%
-  filter(max_val == max(max_val)) %>%
-  select(start, max_val)
-
-max_val_int
 
