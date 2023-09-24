388ca2efff9b95c179b6b1efa0d175988b87215e
Farzy Hafizam <141115682+farzyhafizam@users.noreply.github.com>
2023-09-24 21:01:17 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -48,19 +48,4 @@ collatz_df <- tibble(
 collatz_df
 
 
-#Task 2 --------------------------------
-
-library(tidyverse)
-
-top10longest <- collatz_df %>%
-  arrange(desc(length)) %>%
-  slice(1:10)
-
-top10longest
-
-max_val_int <- collatz_df %>%
-  filter(max_val == max(max_val)) %>%
-  select(start, max_val)
-
-max_val_int
 
