049ebcba16359f06e701262a9fc117ced1e312ef
Farzy Hafizam <141115682+farzyhafizam@users.noreply.github.com>
2023-09-24 20:41:00 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -22,8 +22,8 @@ gen_collatz <- function(n) {
     return(NULL)
   }
   
-    collatz_seq <- c(n)
-    while (n != 1) {
+collatz_seq <- c(n)
+while (n != 1) {
       if (n %% 2 == 0) {
         n <- n / 2
       } else {
@@ -35,28 +35,32 @@ gen_collatz <- function(n) {
     return(collatz_seq)
   }
   
-  start_values <- 1:10000
-  collatz_seqs <- lapply(start_values, gen_collatz)
+start_values <- 1:10000
+collatz_seqs <- lapply(start_values, gen_collatz)
   
-  collatz_df <- tibble(
+collatz_df <- tibble(
     start = start_values,
     seq = collatz_seqs,
     length = sapply(collatz_seqs, length),
     max_val = sapply(collatz_seqs, max)
   )
   
-  collatz_df
+collatz_df
 
 
 #Task 2 --------------------------------
 
+library(tidyverse)
 
+top10longest <- collatz_df %>%
+  arrange(desc(length)) %>%
+  slice(1:10)
 
+top10longest
 
+max_val_int <- collatz_df %>%
+  filter(max_val == max(max_val)) %>%
+  select(start, max_val)
 
-
-
-
-
-
+max_val_int
 
