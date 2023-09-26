1b6bb9b45d32b3d017d91fa26dd21a01c6cbea30
Ak Afdieka Hazimie <141107111+fdkahzme@users.noreply.github.com>
2023-09-26 11:26:22 +0800

    06-creative.R

--- 06-creative.R
+++ 06-creative.R
@@ -1,1 +1,4 @@
+by applying the collatz conjecture rules to a positive integer, we can see that the pattern will always ended up to 4, 2 , 1.
 
+for example:
+we take 5 as starting integer, then by applying the collatz conjecture rule, the pattern will be 16, 8, 4, 2 ,1.
