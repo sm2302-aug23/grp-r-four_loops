71241ed517992d4e294057e5430be0356ff9df08
m1zah <21b9030@ubd.edu.bn>
2023-09-26 09:08:54 +0800

    04-plots.R

--- 04-plots.R
+++ 04-plots.R
@@ -1,1 +1,11 @@
 
+library(ggplot2)
+
+(p1 <- ggplot(data = collatz_df,
+              mapping = aes(x = start,
+                            y = length)) +
+    geom_point() + 
+    labs(
+      x = "Starting Integer",
+      y = "Length of the sequence"
+    ))
