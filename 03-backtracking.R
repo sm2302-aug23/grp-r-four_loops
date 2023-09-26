54e76ba54c6e64734ce4076cc61dd98cc4e2eb0c
Faathimahali <22b9015@ubd.edu.bn>
2023-09-26 10:36:48 +0800

    Update 03-backtracking.R

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -1,8 +1,27 @@
 library(tidyverse)
 library(tibble)
 
+
+backtracks <- function(seq, start) {
+  for(value in seq) {if(value < start) {
+    print("True")
+  } %>%
+    if(value > start) {
+      print("super true")
+    }
+  }
+}
+
+f1 <- apply(X = collatz_df, 
+            MARGIN = 1,
+            FUN = backtracks)
+  
+f1  
+  
+  
 backtracks_df <-
-  filter(.data = collatz_df
-         
+  filter(.data = collatz_df,
+      seq == seq > start %>%
+        
     
   )
